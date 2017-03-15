94f808dda72fa7706680b3dbfd5d49f2c2b81976
Sai Chalasani <niassain@gmail.com>
2017-03-15 15:47:53 -0400

    Added a panel and components to the panel

--- src/GuiExampleMain.java
+++ src/GuiExampleMain.java
@@ -5,8 +5,27 @@ public class GuiExampleMain {
 	public static void main(String[] args) {
 		// creates an instance of JFrame which is the main container box 
 		JFrame frame = new JFrame();
-		//sets the dimensions of the JFrame, height then width
+		//sets the dimensions of the JFrame, width then height
 		frame.setSize(new Dimension(500, 400));
+		//creates a new Jpanel which is what fills up a frame
+		
+		JPanel panel = new JPanel();
+		//this sets the background color of the panel
+		panel.setBackground(Color.BLACK);
+		//this loads the JPanel onto the JFrame
+		// added onto the center of the JFrame and expands to fit by default
+		frame.getContentPane().add(panel);
+		
+		//button is created then added by panel
+		JButton button = new JButton("Button 1");
+		panel.add(button);
+		
+		//empty input textbox is generated and dimensions are set
+		JTextField textField = new JTextField();
+		//this preferred size locks the field to be of that size
+		textField.setPreferredSize(new Dimension(200, 20));
+		panel.add(textField);
+		
 		// This centers the file in the middle, the default is top left corner
 		frame.setLocationRelativeTo(null);
 		// Explains what to do when the X button is pressed, there is another option to close the window but still be active in the process
