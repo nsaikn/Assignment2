73b2e0cd16c77c51ef08e527ce95e82a8e5ea63f
Sai Chalasani <niassain@gmail.com>
2017-03-15 11:21:05 -0400

    commented each line explaing how JFrame works

--- src/GuiExampleMain.java
+++ src/GuiExampleMain.java
@@ -3,12 +3,19 @@ import java.awt.*;
 
 public class GuiExampleMain {
 	public static void main(String[] args) {
+		// creates an instance of JFrame which is the main container box 
 		JFrame frame = new JFrame();
+		//sets the dimensions of the JFrame, height then width
 		frame.setSize(new Dimension(500, 400));
+		// This centers the file in the middle, the default is top left corner
 		frame.setLocationRelativeTo(null);
+		// Explains what to do when the X button is pressed, there is another option to close the window but still be active in the process
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// sets the tile of the window 
 		frame.setTitle("First GUI");
+		//makes sure the GUI is not expandable or shrinkable 
 		frame.setResizable(false);
+		// Makes JFrame visible
 		frame.setVisible(true);
 	}
 }
